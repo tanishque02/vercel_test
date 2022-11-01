9ad8be5bb4f85053ed404463d25dc52905de645c
tanishque02 <87939730+tanishque02@users.noreply.github.com>
2022-11-01 22:36:32 +0530

    Update index.py

--- index.py
+++ index.py
@@ -4,9 +4,5 @@ app = Flask(__name__)
  
 @app.route('/')
 
-def hello_world():
-    return 'Hello World'
- 
-if __name__ == '__main__':
- 
-    app.run()
+def home():
+    return "<h2>Flask Vercel</h2>"
