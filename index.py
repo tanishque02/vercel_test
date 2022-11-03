17ba10116d6401a3ac023e72bbc727b979b23278
tanishque02 <87939730+tanishque02@users.noreply.github.com>
2022-11-03 23:54:44 +0530

    Update index.py

--- index.py
+++ index.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 path = os.listdir()
 
-# wget https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.pbmm
-# wget https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.scorer
- 
-@app.route('/')
+@app.route("/")
 
 def home():
     return path
+
+
+app.run()
