e33906b5915e6f2f3477980ec3e7e4204edac887
tanishque02 <87939730+tanishque02@users.noreply.github.com>
2022-11-03 23:37:24 +0530

    Update index.py

--- index.py
+++ index.py
@@ -4,6 +4,9 @@ import os
 app = Flask(__name__)
 
 path = os.listdir()
+
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.pbmm
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.scorer
  
 @app.route('/')
 
